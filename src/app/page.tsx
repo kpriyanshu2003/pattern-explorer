4ddcb22244d3dff3dd4b7de9ea8923192225fe10
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2024-12-18 09:54:20 +0530

    initia commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@nextui-org/button";
+
+export default function Home() {
+  return (
+    <Button color="primary" className="font-mono">
+      Button
+    </Button>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
+
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+} satisfies Config;
